a6e68eb7c53b4d9ec9c7c2b4d74d57b5cd02db6a
Nathan Ladd <nathanladd@gmail.com>
2025-01-02 08:59:26 -0500

    Project is renamed

--- env.sh
+++ env.sh
@@ -10,13 +10,13 @@ if [ -z "${LIBRARIES_HOME:-}" ]; then
 fi
 
 if [ -z "${LICENSOR:-}" ]; then
-  export LICENSOR="TEMPLATE-LICENSOR"
+  export LICENSOR="Brightworks Digital"
 else
   printf "\e[1mNotice: LICENSOR is overridden: $LICENSOR\e[m\n"
 fi
 
 if [ -z "${GIT_AUTHORITY_PATH:-}" ]; then
-  export GIT_AUTHORITY_PATH="git@github.com:TEMPLATE-GITHUB-ORG"
+  export GIT_AUTHORITY_PATH="git@github.com:test-bench-demo"
 else
   printf "\e[1mNotice: GIT_AUTHORITY_PATH is overridden: $GIT_AUTHORITY_PATH\e[m\n"
 fi
@@ -32,7 +32,7 @@ if [ -z "${RUBYGEMS_PUBLIC_AUTHORITY_ACCESS_KEY:-}" ]; then
 fi
 
 if [ -z "${RUBYGEMS_PRIVATE_AUTHORITY:-}" ]; then
-  export RUBYGEMS_PRIVATE_AUTHORITY="https://rubygems.pkg.github.com/TEMPLATE-GITHUB-ORG"
+  export RUBYGEMS_PRIVATE_AUTHORITY="https://rubygems.pkg.github.com/test-bench-demo"
 else
   printf "\e[1mNotice: RUBYGEMS_PRIVATE_AUTHORITY_PATH is overridden: $RUBYGEMS_PRIVATE_AUTHORITY\e[m\n"
 fi
