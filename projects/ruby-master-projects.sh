34d26b2afc5d42b90ee0f3614c057b6802ebf2da
Nathan Ladd <nathanladd@gmail.com>
2025-01-03 12:38:49 -0500

    TestBench projects are added

--- projects/ruby-master-projects.sh
+++ projects/ruby-master-projects.sh
@@ -1,5 +1,6 @@
 set -e
 
 ruby_master_projects=(
+  master-test-bench
   master-test-bench-fixture
 )
--- projects/ruby-release-gem-projects.sh
+++ projects/ruby-release-gem-projects.sh
@@ -1,6 +1,8 @@
 set -e
 
 ruby_release_gem_projects=(
+  test-bench
+  test-bench-controls
   test-bench-fixture
   test-bench-fixture-controls
 )
