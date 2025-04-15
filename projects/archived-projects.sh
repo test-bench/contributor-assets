58af4d51f82383e9ff790db9a476e1b7a7003686
Nathan Ladd <nathanladd@gmail.com>
2025-04-15 09:25:44 -0400

    test-automated-run, rather than test-run

--- projects/archived-projects.sh
+++ projects/archived-projects.sh
@@ -3,6 +3,7 @@ set -e
 archived_projects=(
   test-fixture
   test-output-writer
+  test-run
   test-session
   test-telemetry
 )
--- projects/ruby-development-gem-projects.sh
+++ projects/ruby-development-gem-projects.sh
@@ -3,8 +3,8 @@ set -e
 ruby_development_gem_projects=(
   test-automated-fixture
   test-automated-output
+  test-automated-run
   test-automated-session
   test-automated-telemetry
   test-cli
-  test-run
 )
